fcc4a0b5a3f1eb390ffb9fda0081a698b9c332f6
Surya Mattu <surya@suryamattu.com>
2014-02-28 16:07:31 -0500

    tidy

--- airodumpParser/airodumpCSVParser.py
+++ airodumpParser/airodumpCSVParser.py
@@ -11,7 +11,7 @@ def parseLine(params,isRouter):
 		if isRouter:
 			node = Node("Router",params)
 			node.trimParams()
-			sender.newNode("Router",node)
+			sender.newNode(node.wrapForOsc())
 			# node.printParams()
 			return node
 			
@@ -19,7 +19,7 @@ def parseLine(params,isRouter):
 		elif not isRouter:
 			node = Node("Client",params)
 			node.trimParams()
-			sender.newNode("Client",node)
+			sender.newNode(node.wrapForOsc())
 			# node.printParams()
 			return node
 			# pass	
