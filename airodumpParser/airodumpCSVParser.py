e205fbfa439b9badd35401b27bba2066b51521e0
Surya Mattu <surya@suryamattu.com>
2014-03-01 02:18:27 -0500

    remove added. messy but it works

--- airodumpParser/airodumpCSVParser.py
+++ airodumpParser/airodumpCSVParser.py
@@ -5,6 +5,10 @@ import time, threading
 routers = dict()
 clients = dict()
 
+routerBSSID = []
+# newrouter
+clientBSSID = []
+
 isRouter = None
 def parseLine(params,isRouter):		
 
@@ -33,11 +37,6 @@ def readFile(fileName):
 		if len(params) <6 :
 			continue
 		
-
-
-		if(line < 2 ):
-			continue
-		
 		if params[0] == "BSSID":
 
 			isRouter = True
@@ -52,56 +51,128 @@ def readFile(fileName):
 		# 	print "ROUTER: "
 		# else:
 		# 	print "CLIENT: "
-		
 		# print params
 		params[2] = params[2].strip()
+		params[0] = params[0].strip()
 		
 		if params[0] in routers and isRouter:
-
+			#collect ID (key) of all exisiting routers
+			routerBSSID.append(params[0])	
+			# print  "HERE"
 			if routers[params[0]].hasTimeChanged(params[2]):
 				# print param
 				# print "Router " +  routers[params[0]].BSSID + " updated"
 				routers[params[0]].updateRouterNode(params)	
-				sender.updateNode(routers[params[0]].wrapForOsc(),params[0],"Router")		
+				sender.updateNode(routers[params[0]].wrapForOsc(),params[0],"Router")
+				
 		
 		elif params[0] in clients and not isRouter:
+
+			#collect ID (key) of all exisiting clients
+			clientBSSID.append(params[0])
+			# print clientBSSID
+			# print  "THERE"
 
 			if clients[params[0]].hasTimeChanged(params[2]):
 
 				# print  "Client " + clients[params[0]] .BSSID+ " updated"
 			 	clients[params[0]].updateClientNode(params)
 				sender.updateNode(clients[params[0]].wrapForOsc(), params[0], "Client")		
+				
+				
+
 		else:
 			if isRouter:
 				n =  parseLine(params,isRouter)
 				routers[n.BSSID] = n
+				routerBSSID.append(params[0])
+				# print routers.keys()
+				print "adding routers" 
 				sender.newNode(n.wrapForOsc(),n.BSSID,"Router")
 			else:
 				n =  parseLine(params,isRouter)
 				clients[n.BSSID] = n
+				clientBSSID.append(params[0])
 				sender.newNode(n.wrapForOsc(),n.BSSID,"Client")
 
 
-		# print len(nodes)
-if __name__ == '__main__' :
+def deleteDeadNodes():
+	
+ 	print len(clientBSSID)  
+ 	print len(routerBSSID) 
+	print clientBSSID 
+	print clients.keys()
+	print  routerBSSID 
+	print routers.keys()
+	
+	clientsCopy = clients;
+	routersCopy = routers;
+	delClients = []
+	delRouters = []
 
+	for k, v in clientsCopy.iteritems():
+	  if k not in clientBSSID:
+	  	print "removeNode " + k
+		sender.removeNode(clients[k].wrapForOsc(),clients[k].BSSID,"Client")
+		delClients.append(k)
+		# del clients[k]	  	
+
+	for k, v in routersCopy.iteritems():
+	  if k not in routerBSSID:
+	  	print "removeClient " + k
+		sender.removeNode(routers[k].wrapForOsc(),routers[k].BSSID,"Router")
+		delRouters.append(k)
+		# del routers[k]	  			
+
+	
+	for k in delClients:
+		del clients[k]
+	
+	for k in delRouters:
+		del routers[k]		
+	# for i in clientBSSID:
+	# 	if i in clients:
+	# 		pass		
+	# 	else:
+	# 		# print "Removing Client ID: " +i
+	# 		sender.removeNode(clients[i].wrapForOsc(),clients[i].BSSID,"Client")
+	# 		del clients[i]
+
+	# for i in routerBSSID:
+	# 	if i in routers:
+	# 		pass
+	# 	else:
+	# 		# print "Removing Router ID: " + i
+	# 		sender.removeNode(routers[i].wrapForOsc(),routers[i].BSSID,"Router")
+	# 		del routers[i]
+		
+	del routerBSSID [:]
+	del clientBSSID [:]
+	print len(clients)
+	print len(routers)
+if __name__ == '__main__' :
+	notFirsTime = False
 	fileName = sys.argv[1]
 	sender = oscSender(8000)
 	try :
 	    while 1 :
 
-			time.sleep(0.5)
+			time.sleep(1)
+			
 			print "test"
 
 			csv = open(fileName, 'r')
 			readFile(csv)
+			if notFirsTime:
+				deleteDeadNodes()
 			csv.close()
+			notFirsTime = True
 
 
 	except KeyboardInterrupt :
 		    print "\nClosing OSCServer."
 		    # mine.s.close()
-		    print "Waiting for Server-thread to finish"
+		    # print "Waiting for Server-thread to finish"
 		    # mine.st.join() ##!!!
 		    print "Done"
 
