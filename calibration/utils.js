21db7cba40ac54782141c001213a31b4545c367f
Surya Mattu <surya@suryamattu.com>
2014-03-17 18:21:36 -0400

    clean up

--- calibration/utils.js
+++ calibration/utils.js
@@ -1,8 +1,6 @@
 function setDuration (numMinutes){
 	var ts = new Date();
-	// console.log( (ts.getMinutes()-numMinutes) >0 ? (ts.getMinutes()-numMinutes) : 0)
 	minutes = (ts.getMinutes()-numMinutes) >0 ? (ts.getMinutes()-numMinutes) : 0
-	// timestamp = ts.getUTCFullYear()+"-"+(ts.getUTCMonth()+1)+"-"+ts.getUTCDate()+" "+ts.getUTCHours()+":"+ minutes	+":"+ts.getUTCSeconds();
 	timestamp = ts.getFullYear()+"-"+(ts.getMonth()+1)+"-"+ts.getDate()+" "+ts.getHours()+":"+ minutes	+":"+ts.getSeconds();
 
 	return timestamp
