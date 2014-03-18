169cb5328fa6d3ed3b2d250782ec3d264b46e057
Surya Mattu <surya@suryamattu.com>
2014-03-18 19:51:02 -0400

    more refactoring

--- calibration/utils.js
+++ calibration/utils.js
@@ -13,8 +13,30 @@ function setDuration (numHours, numMinutes, numSeconds){
 	return timestamp
 }
 
-
 function parseData(data){
+	var nodes = new Array();
+	var links = new Array();
+
+	//TODO: Implement the 'Listeners' in DB. These will be the RPi routers running Airckrack-ng
+	nodes.push({'name' : "Listener", 'power': 1, 'kind': "Listener"});
+
+	for (var i = 0; i < 10; i++) {
+
+		var node = JSON.parse(data[i])
+
+		//Renaming BSSID to name as thats what D3 force stuff expects
+		var n = {'name' : $.trim(node.BSSID), 'power': node.power, 'kind': node.kind};
+		nodes.push(n);
+		link = {'source' : 0, 'target': i, 'power':node.power};
+		links.push(link);
+
+	};
+
+	calibration.data = {'nodes' : nodes, 'links': links}
+}
+
+
+function parseDataWithChildren(data){
 	var nodes = new Array();
 	var links = new Array();
 
@@ -84,6 +106,21 @@ function parseData(data){
 	}
 
 	calibration.data = {'nodes' : nodes, 'links': links}
+}
+
+// Returns a list of all nodes under the root.
+function flatten(root) {
+  var nodes = [], i = 0;
+
+  function recurse(node) {
+    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+    if (!node.id) node.id = ++i;
+    nodes.push(node);
+    return node.size;
+  }
+
+  root.size = recurse(root);
+  return nodes;
 }
 
 var scale = d3.scale.pow()
