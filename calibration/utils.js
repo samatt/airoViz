e5d26006f65a20db1471c483f15bf211299956fe
Surya Mattu <surya@suryamattu.com>
2014-03-18 17:16:00 -0400

    basic visualization

--- calibration/utils.js
+++ calibration/utils.js
@@ -1,7 +1,14 @@
-function setDuration (numMinutes){
-	var ts = new Date();
-	minutes = (ts.getMinutes()-numMinutes) >0 ? (ts.getMinutes()-numMinutes) : 0
-	timestamp = ts.getFullYear()+"-"+(ts.getMonth()+1)+"-"+ts.getDate()+" "+ts.getHours()+":"+ minutes	+":"+ts.getSeconds();
 
+
+function setDuration (numHours, numMinutes, numSeconds){
+
+	var ts = new Date();
+
+	var minutes = (ts.getMinutes()-numMinutes) >0 ? (ts.getMinutes()-numMinutes) : 0
+	var seconds = (ts.getSeconds()-numSeconds) >0 ? (ts.getSeconds()-numSeconds) : 0
+	var hours 	= (ts.getHours()-numHours) >0 ? (ts.getHours()-numHours) : 0
+
+	//FIXME: Get rid off the -1 from getDate
+	timestamp = ts.getFullYear()+"-"+(ts.getMonth()+1)+"-"+(ts.getDate()-1)+" "+hours+":"+ minutes	+":"+seconds;
 	return timestamp
 }
